69fe712bd4710c9702edcab760891d1f6ba70c30
anlisticsData <110004930+anlisticsData@users.noreply.github.com>
2022-09-29 10:32:04 -0300

    carregando pasta do App

---
+++ _app/Config/Agency.inc.php
@@ -0,0 +1,20 @@
+<?php
+
+if (!$WorkControlDefineConf):
+    /*
+      AGENCY DATA
+      Dados da sua agência web
+     */
+    define('AGENCY_CONTACT', 'Danilo S. Peres'); //Nome do contato
+    define('AGENCY_EMAIL', 'danilo@agenciar3.com.br'); //E-mail de contato
+    define('AGENCY_PHONE_A', '(11) 4964-8900'); //Telefone de contato
+    define('AGENCY_PHONE_B', '(11) 4964-0063'); //Telefone de contato
+    define('AGENCY_URL', 'https://www.agenciar3.com.br'); //URL completa do seu site/portfolio
+    define('AGENCY_NAME', 'Agência R3|Fiude Marketing Digital'); //Nome da sua agência web
+    define('AGENCY_ADDR', 'Rua Santo Antônio do Aventureiro, 114, Sala 02'); //Endereço da sua agência web (RUA, NÚMERO)
+    define('AGENCY_CITY', 'Guarulhos');  //Endereço da sua agência web (CIDADE)
+    define('AGENCY_UF', 'SP');  //Endereço da sua agência web (UF DO ESTADO)
+    define('AGENCY_ZIP', '07143-130');  //Endereço da sua agência web (CEP)
+    define('AGENCY_COUNTRY', 'Brasil');  //Endereço da sua agência web (PAÍS)
+endif;
+
---
+++ _app/Config/Client.inc.php
@@ -0,0 +1,66 @@
+<?php
+
+if (!$WorkControlDefineConf):
+    /*
+     * SITE CONFIG
+     */
+    define('SITE_NAME', 'Inforcomp'); //Nome do site do cliente
+    define('SITE_SUBNAME', 'Tecnologia em soluções administrativas'); //Nome do site do cliente
+    define('SITE_DESC', 'Fundada em janeiro de 1982 com o objetivo de prestar consultoria na área de informática a Inforcomp consolidou-se no desenvolvimento de Softwares da área de RH.'); //Descrição do site do cliente
+
+    /*
+     * SHIP CONFIG
+     * DADOS DO SEU CLIENTE/DONO DO SITE
+     */
+    define('SITE_ADDR_NAME', 'Inforcomp'); //Nome de remetente
+    define('SITE_ADDR_RS', ''); //Razão Social
+    define('SITE_ADDR_EMAIL', 'inforcomp@inforcomp.com.br'); //E-mail de contato
+    define('SITE_ADDR_SITE', ''); //URL descrita
+    define('SITE_ADDR_CNPJ', ''); //CNPJ da empresa
+    define('SITE_ADDR_IE', ''); //Inscrição estadual da empresa
+    define('SITE_ADDR_PHONE_A', 'Tel: (11) 2226-3600'); //Telefone 1
+    define('SITE_ADDR_PHONE_B', ''); //Telefone 2
+    define('SITE_ADDR_WHATSAPP', ''); //Whats App
+    define('SITE_ADDR_ADDR', 'Rua força pública nº 72'); //ENDEREÇO: rua, número (complemento)
+    define('SITE_ADDR_CITY', 'São Paulo'); //ENDEREÇO: cidade
+    define('SITE_ADDR_DISTRICT', 'Bairro Santana'); //ENDEREÇO: bairro
+    define('SITE_ADDR_UF', 'SP'); //ENDEREÇO: UF do estado
+    define('SITE_ADDR_ZIP', '1'); //ENDEREÇO: CEP
+    define('SITE_ADDR_COUNTRY', 'Brasil'); //ENDEREÇO: País
+
+
+    /**
+     * Social Config
+     */
+    define('SITE_SOCIAL_NAME', '');
+
+    /*
+     * Google
+     */
+    define('SITE_SOCIAL_GOOGLE', 1); //0 caso não seja utilizado para o cliente
+    define('SITE_SOCIAL_GOOGLE_AUTHOR', ''); //https://plus.google.com/????? (**ID DO USUÁRIO)
+    define('SITE_SOCIAL_GOOGLE_PAGE', ''); //https://plus.google.com/???? (**ID DA PÁGINA)
+    /*
+     * Facebook
+     */
+    define('SITE_SOCIAL_FB', 1); //0 caso não seja utilizado para o cliente
+    define('SITE_SOCIAL_FB_APP', 0); //Opcional APP do facebook
+    define('SITE_SOCIAL_FB_AUTHOR', ''); //https://www.facebook.com/?????
+    define('SITE_SOCIAL_FB_PAGE', ''); //https://www.facebook.com/?????
+    /*
+     * Twitter
+     */
+    define('SITE_SOCIAL_TWITTER', ''); //https://www.twitter.com/?????
+    /*
+     * YouTube
+     */
+    define('SITE_SOCIAL_YOUTUBE', ''); //https://www.youtube.com/user/?????
+    /*
+     * Instagram
+     */
+    define('SITE_SOCIAL_INSTAGRAM', ''); //https://www.instagram.com/?????
+    /*
+     * Snapchat
+     */
+    define('SITE_SOCIAL_SNAPCHAT', ''); //https://www.snapchat.com/add/?????
+endif;
